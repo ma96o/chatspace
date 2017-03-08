9e4a75d32d3708cad4f2b8f21da0c1487a904bff
masaaki1915 <masaaki1915@gmail.com>
2017-03-08 20:46:09 +0900

    application.rbにconfig.generetors 追記

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,20 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec,
+                      view_specs: false,
+                      helper_specs: false,
+                      fixture: true,
+                      fixture_replacement: :factory_girl,
+                      routing_specs: false,
+                      integration_tool: false
+    end
   end
 end
+
+
+
