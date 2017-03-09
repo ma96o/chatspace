39638ab13fdb98c41e88bacf6eb3287b7aefbe4e
masaaki1915 <masaaki1915@gmail.com>
2017-03-09 22:17:00 +0900

    routing error が起きたためrootを最上部へ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'top#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
-  root 'top#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
