79536340073f8f19ef555f098bbea4c5f37d48f8
masaaki1915 <masaaki1915@gmail.com>
2017-03-10 12:09:34 +0900

    configure_permitted_parametersでnameの値もsign_up時に受け取れるようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:name)
+  end
 end
+
