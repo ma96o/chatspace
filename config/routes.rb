da1f79b03b020cc6c8c71489acaab53e7ace5fc3
masaaki1915 <masaaki1915@gmail.com>
2017-03-10 12:21:01 +0900

    devise_forのcontrollerオプション削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root 'top#index'
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
