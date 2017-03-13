e23447a090612b3e20b572328f173085fe86f89b
masaaki1915 <masaaki1915@gmail.com>
2017-03-13 23:07:19 +0900

    groupsとusersのアソシエーション定義

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,3 @@
 class Group < ApplicationRecord
-  has_many :users
+  has_many :users, through: :users_groups
 end
--- app/models/usersgroup.rb
+++ app/models/usersgroup.rb
@@ -1,2 +1,4 @@
 class Usersgroup < ApplicationRecord
+  belongs_to :user
+  belongs_to :group
 end
