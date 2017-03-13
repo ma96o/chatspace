d6f24c7f3fc8aeb0ca802dd829c2eed83344e969
masaaki1915 <masaaki1915@gmail.com>
2017-03-13 10:30:52 +0900

    resourcesの配列かっこよく描く

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
   devise_for :users
-  resources :groups, only: [:new, :edit]
+  resources :groups, only: %i(new edit)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
