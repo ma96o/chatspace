805102103fbfbf279da43d81906acf0e1c40700f
masaaki1915 <masaaki1915@gmail.com>
2017-03-14 18:44:35 +0900

    path書き忘れ

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
   def create
     group = Group.create(name: group_params[:name])
     group_params[:user_ids].map{ |user_id| group.users_groups.create(user_id: user_id) if user_id != "" }
-    redirect_to root
+    redirect_to root_path
   end
 
   def edit ; end
