d622b59248c03109060862ad8bbe6e6578a6c05c
masaaki1915 <masaaki1915@gmail.com>
2017-03-14 18:21:44 +0900

    groups#createのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'top#index'
   devise_for :users
   resources :groups, only: %i(new edit)
+  post "/chat_groups" => "groups#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
