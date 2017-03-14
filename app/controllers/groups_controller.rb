729841c5a820582714b9e3ca615d605ad6aa5e66
masaaki1915 <masaaki1915@gmail.com>
2017-03-14 18:41:42 +0900

    groups_controllerでcreateアクションを定義

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,16 @@
 class GroupsController < ApplicationController
   def new ; end
 
+  def create
+    group = Group.create(name: group_params[:name])
+    group_params[:user_ids].map{ |user_id| group.users_groups.create(user_id: user_id) if user_id != "" }
+    redirect_to root
+  end
+
   def edit ; end
+
+  private
+  def group_params
+    params.require(:chat_group).permit(:name, user_ids: [])
+  end
 end
