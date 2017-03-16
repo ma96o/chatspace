245317be4999af5475955ce82e57faeeb5cbc549
masaaki1915 <masaaki1915@gmail.com>
2017-03-16 13:44:53 +0900

    groups#updateルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'top#index'
   devise_for :users
-  resources :groups, only: %i(new edit)
-  post "/chat_groups" => "groups#create"
+  resources :groups, only: %i(new create edit update)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
