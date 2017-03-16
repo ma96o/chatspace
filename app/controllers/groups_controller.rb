36e3781e8ca9901a6996606be4d9ba007c92a911
masaaki1915 <masaaki1915@gmail.com>
2017-03-16 17:07:10 +0900

    groups#createのredirect_to部分編集

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,14 +4,15 @@ class GroupsController < ApplicationController
   end
 
   def create
-    group = Group.create(group_params)
-    redirect_to :back, flash: { alert: "グループ生成に失敗しました" } and return if group.nil?
-    redirect_to root_path
+    group = Group.new(group_params)
+    redirect_to :back, flash: { alert: "グループ生成に失敗しました" } and return if group.save
+    render "top/index"
   end
 
   def edit ; end
 
   private
+
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
