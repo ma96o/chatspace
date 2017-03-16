e1636af68a103e3637f8267ef372f975ab36c85c
masaaki1915 <masaaki1915@gmail.com>
2017-03-16 23:26:01 +0900

    message#createアクション定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,17 @@
 class MessagesController < ApplicationController
+  def create
+    message = Message.new(message_params)
+    if message.save
+      redirect_to root_path
+    else
+      flash.now[:alert] = "メッセージの送信に失敗しました"
+      render "top#index"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text, :user_id)
+  end
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,6 @@
 class TopController < ApplicationController
   def index
+    @message = Message.new
+    @message.user_id = current_user.id
   end
 end
