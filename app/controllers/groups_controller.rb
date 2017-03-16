045e3e3bf3cb2dc7670c18a9668587a9c107cd6d
masaaki1915 <masaaki1915@gmail.com>
2017-03-16 09:54:21 +0900

    groups#createのcreateメソッドの引数にそのままparamsを入れた

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,8 +2,7 @@ class GroupsController < ApplicationController
   def new ; end
 
   def create
-    group = Group.create(name: group_params[:name])
-    group_params[:user_ids].map{ |user_id| group.users_groups.create(user_id: user_id) if user_id != "" }
+    group = Group.create(group_params)
     redirect_to root_path
   end
 
