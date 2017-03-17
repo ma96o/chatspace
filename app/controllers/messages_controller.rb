dfac8c0c0128d40fb66c2c366c106d9eb80772c8
masaaki1915 <masaaki1915@gmail.com>
2017-03-17 17:22:54 +0900

    messages#create処理完成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,28 +4,34 @@ class MessagesController < ApplicationController
       friend = User.find(params[:user_id]) # メッセージを受け取るユーザ
       friend_messages = current_user.messages.where(user_id: params[:id])
       my_messages = friend.messages.where(user_id: current_user)
-      @messages = my_messages.to_a.concat(opp_messages).sort! { |a, b| b[:created_at] <=> a[:created_at] }
+      @messages = my_messages.to_a.concat(friend_messages).sort! { |a, b| b[:created_at] <=> a[:created_at] }
     elsif params[:group_id]
       group = Group.find(params[:group_id])
       @messages = group.messages
     end
     @message = Message.new
-    @message.user_id = current_user
   end
 
   def create
-    message = Message.new(message_params)
+    if params[:user_id]
+      friend = User.find(params[:user_id])
+      message = friend.messages.new(message_params)
+    elsif params[:group_id]
+      group = Group.find(params[:group_id])
+      message = group.messages.new(message_params)
+    end
+
     if message.save
-      redirect_to "index"
+      redirect_to action: :index and return
     else
       flash.now[:alert] = "メッセージの送信に失敗しました"
-      render "index"
+      render "index" and return
     end
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:text).merge(:user_id)
+    params.require(:message).permit(:text).merge(user_id: current_user.id)
   end
 end
