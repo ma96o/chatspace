de543625caf7cf024dd4cea2a4301eea1a1ee4ad
masaaki1915 <masaaki1915@gmail.com>
2017-03-17 11:01:03 +0900

    messages#indexルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'top#index'
   devise_for :users
   resources :groups, except: %i(index delete show)
-  resources :messages, only: :create, as: :messages
+  resources :messages, only: :create
+  get ':type/:id' => 'messages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
