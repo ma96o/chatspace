5823a9ee1752aeecb23a6a4ed1d9379e8332d5f8
masaaki1915 <masaaki1915@gmail.com>
2017-03-17 16:18:50 +0900

    messages#indexルーティングの変更に伴い条件分岐の変更と、変数名の変更

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,19 @@
 class MessagesController < ApplicationController
   def index
-    if params[:type] == "user"
-
-      opponent = User.find(params[:id])
-      opp_messages = current_user.messages.where(user_id: params[:id])
-      my_messages = opponent.messages.where(user_id: current_user)
+    if params[:user_id]
+      friend = User.find(params[:user_id]) # メッセージを受け取るユーザ
+      friend_messages = current_user.messages.where(user_id: params[:id])
+      my_messages = friend.messages.where(user_id: current_user)
       @messages = my_messages.to_a.concat(opp_messages).sort! { |a, b| b[:created_at] <=> a[:created_at] }
-
-    elsif params[:type] == "group"
-
-      opponent = Group.find(params[:id])
-      @messages = opponent.messages
-
+    elsif params[:group_id]
+      group = Group.find(params[:group_id])
+      @messages = group.messages
     end
-
     @message = Message.new
     @message.user_id = current_user
   end
 
   def create
-
     message = Message.new(message_params)
     if message.save
       redirect_to "index"
