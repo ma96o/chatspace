5b7dc9b044f2757863c36bc09d6d427002fe2319
masaaki1915 <masaaki1915@gmail.com>
2017-03-17 17:33:28 +0900

    message#index&create内の処理メソッド化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,12 @@
 class MessagesController < ApplicationController
   def index
-    if params[:user_id]
-      friend = User.find(params[:user_id]) # メッセージを受け取るユーザ
-      friend_messages = current_user.messages.where(user_id: params[:id])
-      my_messages = friend.messages.where(user_id: current_user)
-      @messages = my_messages.to_a.concat(friend_messages).sort! { |a, b| b[:created_at] <=> a[:created_at] }
-    elsif params[:group_id]
-      group = Group.find(params[:group_id])
-      @messages = group.messages
-    end
+    @messages = set_messages
+
     @message = Message.new
   end
 
   def create
-    if params[:user_id]
-      friend = User.find(params[:user_id])
-      message = friend.messages.new(message_params)
-    elsif params[:group_id]
-      group = Group.find(params[:group_id])
-      message = group.messages.new(message_params)
-    end
+    message = set_message
 
     if message.save
       redirect_to action: :index and return
@@ -33,5 +20,29 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:text).merge(user_id: current_user.id)
+  end
+
+  def set_message
+    if params[:user_id]
+      friend = User.find(params[:user_id])
+      message = friend.messages.new(message_params)
+    elsif params[:group_id]
+      group = Group.find(params[:group_id])
+      message = group.messages.new(message_params)
+    end
+    return message
+  end
+
+  def set_messages
+    if params[:user_id]
+      friend = User.find(params[:user_id]) # メッセージを受け取るユーザ
+      friend_messages = current_user.messages.where(user_id: params[:id])
+      my_messages = friend.messages.where(user_id: current_user)
+      @messages = my_messages.to_a.concat(friend_messages).sort! { |a, b| b[:created_at] <=> a[:created_at] }
+    elsif params[:group_id]
+      group = Group.find(params[:group_id])
+      @messages = group.messages
+    end
+    return @messages
   end
 end
