a4d69d252f0109f1aab78c86f828318be5d4ee90
masaaki1915 <masaaki1915@gmail.com>
2017-03-18 11:42:46 +0900

    messages#index&createアクション定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,9 @@
 class MessagesController < ApplicationController
+  def index
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+    @message = Message.new
+  end
+
+  def create ; end
 end
