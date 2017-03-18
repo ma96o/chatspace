5b4d4c32b87960cc4e3078250917ed47ccc2b6d0
masaaki1915 <masaaki1915@gmail.com>
2017-03-18 15:00:16 +0900

    messages#create処理記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @groups = Group.all
     @group = Group.find(params[:group_id])
     @messages = @group.messages
     @message = Message.new
   end
 
-  def create ; end
+  def create
+    message = Message.new(message_params)
+    if message.save
+      redirect_to group_messages_path
+    else
+      flash.now[:alert] = "メッセージの送信に失敗しました"
+      render "index"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
 end
