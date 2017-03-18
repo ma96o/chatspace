4c2b1cfc28670ce1f164bbbf90c3164cdb126a13
masaaki1915 <masaaki1915@gmail.com>
2017-03-18 19:23:51 +0900

    messages#create時のフラッシュメッセージ実装 インスタンス変数定義をメソッド化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,19 @@
 class MessagesController < ApplicationController
+  before_action :set_groups, only: %i(index create)
+  before_action :set_group, only: %i(index create)
+  before_action :set_messages, only: %i(index create)
+
   def index
-    @groups = current_user.groups
-    @group = Group.find(params[:group_id])
-    @messages = @group.messages
     @message = Message.new
   end
 
   def create
-    message = Message.new(message_params)
-    if message.save
-      flash.now[:notice] = "メッセージを送信しました"
-      redirect_to group_messages_path
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path, notice: "メッセージを送信しました"
     else
       flash.now[:alert] = "メッセージの送信に失敗しました"
-      render "index"
+      render action: :index, alert: "メッセージの送信に失敗しました"
     end
   end
 
@@ -21,5 +21,17 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:text).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def set_groups
+    @groups = current_user.groups
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def set_messages
+    @messages = @group.messages
   end
 end
