2c8498a66b3c013f9f7910127b13d535a821a2cb
masaaki1915 <masaaki1915@gmail.com>
2017-03-21 17:11:37 +0900

    message.js可読性上げるために改行追加 console.log削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,15 @@
 $(function() {
   function buildHTML(message) {
-    console.log(message)
     var userName = $('#side-bar .header p').text();
-    var html = $('<li class="message">').append('<p class="message__name">' + userName + '<span>' + message.created_at  + '</span></p><p class="message__text">' + message.text + '</p>');
+    var html = $('<li class="message">').append(`<p class="message__name">
+                                                   ${userName}
+                                                   <span>
+                                                     ${message.created_at}
+                                                   </span>
+                                                 </p>
+                                                 <p class="message__text">
+                                                   ${message.text}
+                                                 </p>`);
     return html;
   };
 
