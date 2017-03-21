fe421b9df2cc5b94a8f4b56a239bc7726f3afec9
masaaki1915 <masaaki1915@gmail.com>
2017-03-21 17:26:10 +0900

    セレクタの整理 .fail()削除

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function() {
   function buildHTML(message) {
-    var userName = $('#side-bar .header p').text();
+    var userName = $('#user-name').text();
     var html = $('<li class="message">').append(`<p class="message__name">
                                                    ${userName}
                                                    <span>
@@ -13,9 +13,9 @@ $(function() {
     return html;
   };
 
-  $('.form-wrapper form').on('submit', function(e) {
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
-    var textField = $('.form-wrapper  form .text');
+    var textField = $('#message_text');
     var message = textField.val();
     var s = $(location).attr('pathname');
     s = s.split('/');
@@ -31,10 +31,8 @@ $(function() {
     })
     .done(function(data) {
       var html = buildHTML(data);
-      $('.container ul').append(html);
-      textField.val();
+      $('#messages').append(html);
+      textField.val("");
     })
-    .fail(function() {
-    });
   });
 });
