da68f48bf70137d27203f71aa40fd539e7d7e5e9
masaaki1915 <masaaki1915@gmail.com>
2017-03-21 21:22:24 +0900

    messagesストロングパラメータでimageキーの値も取得するように指定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:text).merge(user_id: current_user.id, group_id: params[:group_id])
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 
   def set_groups
