833090e45aa4667db79a363b0196386e8bcb5561
masaaki1915 <masaaki1915@gmail.com>
2017-03-21 14:12:58 +0900

    messages#createで結果をjsonで返す

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,14 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_path, notice: "メッセージを送信しました"
+      respond_to do |format|
+        format.html {
+          redirect_to group_messages_path(@group), notice: "メッセージを送信しました"
+        }
+        format.json {
+          render json: @message
+        }
+      end
     else
       flash.now[:alert] = "メッセージの送信に失敗しました"
       render action: :index
