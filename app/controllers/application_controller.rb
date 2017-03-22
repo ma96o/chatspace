81b69e59a2eda36872f45a242b8f2a2f8ecb3ac9
masaaki1915 <masaaki1915@gmail.com>
2017-03-22 12:41:39 +0900

    ログイン後グループチャット画面へ遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    group_messages_path(current_user.groups.first)
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
