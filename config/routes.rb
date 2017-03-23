ebe2d82adf58b276227ccad8bbfed33d84099997
masaaki1915 <masaaki1915@gmail.com>
2017-03-23 22:33:07 +0900

    group#ajax_user_listルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :groups, except: %i(index delete show) do
     resources :messages, only: %i(index create)
+    collection do
+      get :ajax_user_list
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
