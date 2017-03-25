889d8dbec0e00b7df6a25f8c5da77f3a5915c4da
masaaki1915 <masaaki1915@gmail.com>
2017-03-25 13:30:24 +0900

    画像投稿非同期実装

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,6 +1,11 @@
 $(function() {
   function buildHTML(message) {
     var userName = $('#user-name').text();
+    if (!message.image) {
+      var messageImage = ``;
+    } else {
+      var messageImage = `<img src="${message.image}" alt="Plofile fb n">`;
+    };
     var html = $('<li class="message">').append(
           `<p class="message__name">
              ${userName}
@@ -10,28 +15,29 @@ $(function() {
            </p>
            <p class="message__text">
              ${message.text}
-           </p>`
+           </p>
+           ${messageImage}`
           );
     return html;
   };
 
   $('#new_message').on('submit', function(e) {
+    var $form = this;
     e.preventDefault();
     var textField = $('#message_text');
-    var message = textField.val();
+    var formData = new FormData($(this)[0]);
     $.ajax({
       type: 'POST',
       url: './messages.json',
-      data: {
-        message: {
-          text: message
-        }
-      }
+      processData: false,
+      contentType: false,
+      data: formData
     })
     .done(function(data) {
       var html = buildHTML(data);
       $('#messages').append(html);
       textField.val("");
+      $form.reset();
     })
   });
 
