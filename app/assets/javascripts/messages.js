08ac1d7f5e6e099eadd415aebaef0119b3b5e719
masaaki1915 <masaaki1915@gmail.com>
2017-03-25 13:29:47 +0900

    pull master 2

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,41 @@
+$(function() {
+  function buildHTML(message) {
+    var userName = $('#user-name').text();
+    var html = $('<li class="message">').append(
+          `<p class="message__name">
+             ${userName}
+             <span>
+               ${message.created_at}
+             </span>
+           </p>
+           <p class="message__text">
+             ${message.text}
+           </p>`
+          );
+    return html;
+  };
+
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var textField = $('#message_text');
+    var message = textField.val();
+    $.ajax({
+      type: 'POST',
+      url: './messages.json',
+      data: {
+        message: {
+          text: message
+        }
+      }
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('#messages').append(html);
+      textField.val("");
+    })
+  });
+
+  // setTimeout(function() {
+  //   location.reload();
+  // }, 10000);
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,2 @@
+json.set! :text, @message.text
+json.set! :created_at, @message.created_at.strftime('%Y年%m月%d日 %H:%M:%S')
