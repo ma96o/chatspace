007fc7eb131dbc1f467d179851e32d0cced388a2
masaaki1915 <masaaki1915@gmail.com>
2017-03-25 14:50:40 +0900

    画像投稿ビュー完成

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
 
-  validates_presence_of :text, :user_id, :group_id
+  validates_presence_of :user_id, :group_id
+
+  mount_uploader :image, ImageUploader
 end
