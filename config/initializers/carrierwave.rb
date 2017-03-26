829270a62e02d0adf3f43e10838703252ea1745e
masaaki1915 <masaaki1915@gmail.com>
2017-03-26 17:20:08 +0900

    s3バケット変更&環境変数変更&case文修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,16 +4,17 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   case Rails.env
-    when 'production' || 'development'
+    when 'production', 'development'
       config.storage = :fog
+      config.fog_provider = 'fog/aws'
       config.fog_credentials = {
         provider: 'AWS',
-        aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
-        region: 'ap-northeast-1'
+        aws_access_key_id: ENV['ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+        region: 'us-east-1'
       }
-      config.fog_directory = 'chat-space-images-mk'
-      config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-images-mk'
+      config.fog_directory = 'chat-space-images-k'
+      config.asset_host = 'https://s3.amazonaws.com/chat-space-images-k'
     when 'test'
       config.storage = :file
   end
