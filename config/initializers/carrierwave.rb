b0c43d12a1d7f7ee5951184c9ead44d8088d7f45
masaaki1915 <masaaki1915@gmail.com>
2017-03-26 18:14:20 +0900

    case文のインデント修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,18 +4,17 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   case Rails.env
-    when 'production', 'development'
-      config.storage = :fog
-      config.fog_provider = 'fog/aws'
-      config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-        region: 'us-east-1'
-      }
-      config.fog_directory = 'chat-space-images-k'
-      config.asset_host = 'https://s3.amazonaws.com/chat-space-images-k'
-    when 'test'
-      config.storage = :file
+  when 'production', 'development'
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
+      region: 'us-east-1'
+    }
+    config.fog_directory = 'chat-space-images-k'
+    config.asset_host = 'https://s3.amazonaws.com/chat-space-images-k'
+  when 'test'
+    config.storage = :file
   end
 end
