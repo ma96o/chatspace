0de505d1a1df1295d6fcd6a96b8e62f908a2c56f
masaaki1915 <masaaki1915@gmail.com>
2017-03-27 12:51:49 +0900

    textのみ、imageのみmessageのテスト追記

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,13 +3,25 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
 
-    it 'can not save caught in validation' do
-      message = build(:message, text: "", image: "")
+    it 'is valid with a text ' do
+      message = build(:message, image: "")
       message.valid?
-      expect(message.errors[:message][0]).to include("を入力してください")
+      expect(message).to be_valid
     end
 
-    it 'saved without caught in validation' do
+    it 'is valid with an image' do
+      message = build(:message, text: "")
+      message.valid?
+      expect(message).to be_valid
+    end
+
+    it 'is invalid without a text & an image' do
+      message = build(:message, text: "", image: "")
+      message.valid?
+      expect(message.errors[:message][0]).to include("メッセージを入力してください")
+    end
+
+    it 'is valid with all attributes' do
       message = build(:message)
       message.valid?
       expect(message).to be_valid
