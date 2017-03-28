707f8aaca14dc33be07b2084a2df6534e08b4db9
masaaki1915 <masaaki1915@gmail.com>
2017-03-28 21:16:19 +0900

    messages_controllerテストコード修正

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -24,18 +24,39 @@ describe MessagesController do
   end
 
   describe 'POST #create' do
+
     context 'success to save' do
-      it 'save success' do
-        expect {
-          post :create, params: {group_id: group.id, user_id: user.id, message: attributes_for(:message)}
-        }.to change(Message, :count)
+      context 'when format is html' do
+        it 'save success' do
+          expect {
+            post :create, params: {group_id: group.id, user_id: user.id, message: attributes_for(:message), format: :html}
+          }.to change(Message, :count).by(1)
+        end
+
+        it 'redirect to #index' do
+          post :create, params: {group_id: group.id, user_id: user.id, message: attributes_for(:message), format: :html}
+          expect(response).to redirect_to group_messages_path(group.id)
+        end
+
+        it 'has success message' do
+          post :create, params: {group_id: group.id, user_id: user.id, message: attributes_for(:message), format: :html}
+          expect(flash[:notice]).to be_present
+        end
       end
 
-      it 'redirects to messages#index' do
-        post :create, group_id: group.id, user_id: user.id, message: attributes_for(:message)
-        expect(response).to redirect_to group_messages_path(group.id)
+      context 'when format is json' do
+        it 'save success' do
+          expect {
+            post :create, params: {group_id: group.id, user_id: user.id, message: attributes_for(:message), format: :json}
+          }.to change(Message, :count).by(1)
+        end
+        it 'no render template' do
+          post :create, params: {group_id: group.id, user_id: user.id, message: attributes_for(:message), format: :json}
+          expect(@template).to be_nil
+        end
       end
     end
+
 
     context 'fail to save' do
       it 'can not save in the database' do
@@ -44,9 +65,14 @@ describe MessagesController do
         }.not_to change(Message, :count)
       end
 
-      it 'response as ajax' do
+      it 'render #index after save failed' do
         post :create, params: {group_id: group.id,  user_id: user.id, message: attributes_for(:message, text: nil, image: nil)}
-        expect(response.status).to eq 200
+        expect(response).to render_template(:index)
+      end
+
+      it 'has alert message' do
+        post :create, params: {group_id: group.id,  user_id: user.id, message: attributes_for(:message, text: nil, image: nil)}
+        expect(flash[:alert]).to be_present
       end
     end
   end
