24b4d02994cbb5358559d1762946d33bb6499827
masaaki1915 <masaaki1915@gmail.com>
2017-03-30 18:08:15 +0900

    Capistranoの設定ファイル編集

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+# config valid only for current version of Capistrano
+lock '<Capistranoのバージョン>'
+
+set :application, 'chatspace'
+set :repo_url, 'git@github.com:masaaki1915/chatspace.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['/Users/kubo/.ssh/key_pair.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
